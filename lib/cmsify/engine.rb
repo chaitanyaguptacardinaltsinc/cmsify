011b80c0dfd443218ff834f79089abd50650d385
Niraj Kumar <niraj.kumar@cardinaltsinc.net>
2025-02-11 21:15:42 +0530

    fix issue

--- lib/cmsify/engine.rb
+++ lib/cmsify/engine.rb
@@ -1,10 +1,24 @@
 module Cmsify
   class Engine < ::Rails::Engine
-    config.autoload_paths += Dir["#{config.root}/lib/**/"]
     isolate_namespace Cmsify
 
-    config.to_prepare do
-      Cmsifier.cmsify
+    # Update autoloading configuration for Zeitwerk
+    initializer "cmsify.autoload", before: :set_autoload_paths do |app|
+      app.config.autoload_paths << "#{root}/lib"
+      app.config.eager_load_paths << "#{root}/lib"
+      
+      # If you have nested directories in lib that need autoloading
+      Dir["#{root}/lib/**/"].each do |path|
+        app.config.autoload_paths << path
+        app.config.eager_load_paths << path
+      end
+    end
+
+    # Move the cmsifier initialization to after autoloading is set up
+    config.after_initialize do
+      Rails.application.config.to_prepare do
+        Cmsifier.cmsify
+      end
     end
   end
 end
