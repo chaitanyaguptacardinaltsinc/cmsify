4657d99fd187a87f9d2492d8d25f6456cdc71e06
Niraj Kumar <niraj.kumar@cardinaltsinc.net>
2025-02-12 14:43:12 +0530

    fix issue

--- lib/cmsify.rb
+++ lib/cmsify.rb
@@ -18,19 +18,8 @@ require "cocoon"
 require "active_model_serializers"
 require "recursive-open-struct"
 require "font-awesome-rails"
-require_relative "cmsify/configuration"
-require_relative "cmsify/klass" if File.exist?(File.join(__dir__, 'cmsify/klass.rb'))
-require_relative "cmsify/schemable"
-require_relative "cmsify/cmsified"
-begin
-  require 'rails/engine'
-  require 'cmsify/engine'
-  rescue LoadError
-end
 
 module Cmsify
-  extend ActiveSupport::Autoload
-
   RESOURCE_TYPES = [:item, :collection]
 
   class << self
@@ -55,6 +44,16 @@ module Cmsify
   def self.root
     File.dirname __dir__
   end
+end
+# Require files in dependency order
+require_relative "cmsify/configuration"
+require_relative "cmsify/schemable" if File.exist?(File.join(__dir__, 'cmsify/schemable.rb'))
+require_relative "cmsify/cmsified" if File.exist?(File.join(__dir__, 'cmsify/cmsified.rb'))
+
+begin
+  require 'rails/engine'
+  require_relative 'cmsify/engine'
+rescue LoadError
 end
 
 ActiveSupport.on_load(:active_record) do
